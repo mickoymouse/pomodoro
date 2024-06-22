0215b7ba9939685e2087f258c5d05bb3432e6f98
mickoymouse <mckslvstr@gmail.com>
2024-06-22 16:28:39 +0800

    Branched out timer design and implemented tabs for pomodor, shortbreak, and long break.

--- app/page.tsx
+++ app/page.tsx
@@ -18,18 +18,17 @@ enum PomodoroState {
 }
 
 export default function Home() {
-	// const radius = 169.5;
-	// const dashArray = radius * 2 * Math.PI;
-
 	const [radius, setRadius] = useState(169.5);
 	const [dashArray, setDashArray] = useState(radius * 2 * Math.PI);
-	const [pomodoroDuration, setPomodoroDuration] = useState(1);
 
 	useEffect(() => {
 		setDashArray(radius * 2 * Math.PI);
 	}, [radius]);
 
+	const [tab, setTab] = useState(0);
+
 	// Pomodoro Timer
+	const [pomodoroDuration, setPomodoroDuration] = useState(25);
 	const {
 		timer: pomodoroTimer,
 		isVisible: pomodoroIsVisible,
@@ -40,6 +39,32 @@ export default function Home() {
 		setTimerState: setPomodoroState,
 		setTimer: setPomodoroTimer,
 	} = useTimer(dashArray, pomodoroDuration);
+
+	// short break timer
+	const [shortBreakDuration, setShortBreakDuration] = useState(5);
+	const {
+		timer: shortBreakTimer,
+		isVisible: shortBreakIsVisible,
+		initialTimer: shortBreakInitialTimer,
+		timerState: shortBreakState,
+		dashOffset: shortBreakDashOffset,
+		setIsRunning: setShortBreakIsRunning,
+		setTimerState: setShortBreakState,
+		setTimer: setShortBreakTimer,
+	} = useTimer(dashArray, shortBreakDuration);
+
+	// long break timer
+	const [longBreakDuration, setLongBreakDuration] = useState(15);
+	const {
+		timer: longBreakTimer,
+		isVisible: longBreakIsVisible,
+		initialTimer: longBreakInitialTimer,
+		timerState: longBreakState,
+		dashOffset: longBreakDashOffset,
+		setIsRunning: setLongBreakIsRunning,
+		setTimerState: setLongBreakState,
+		setTimer: setLongBreakTimer,
+	} = useTimer(dashArray, longBreakDuration);
 
 	const formatTime = (time: number) => {
 		console.log(pomodoroDuration);
@@ -60,11 +85,62 @@ export default function Home() {
 			case TimerState.PAUSED:
 				setPomodoroIsRunning(true);
 				setPomodoroState(TimerState.RUNNING);
+
+				setShortBreakIsRunning(false);
+				setShortBreakState(TimerState.PAUSED);
+				setLongBreakIsRunning(false);
+				setLongBreakState(TimerState.PAUSED);
 				break;
 			case TimerState.FINISHED:
 				setPomodoroTimer(pomodoroInitialTimer);
 				setPomodoroIsRunning(true);
 				setPomodoroState(TimerState.RUNNING);
+				break;
+		}
+	};
+
+	const manageShortBreakTimer = (state: TimerState) => {
+		switch (state) {
+			case TimerState.RUNNING:
+				setShortBreakIsRunning(false);
+				setShortBreakState(TimerState.PAUSED);
+				break;
+			case TimerState.PAUSED:
+				setShortBreakIsRunning(true);
+				setShortBreakState(TimerState.RUNNING);
+
+				setPomodoroIsRunning(false);
+				setPomodoroState(TimerState.PAUSED);
+				setLongBreakIsRunning(false);
+				setLongBreakState(TimerState.PAUSED);
+				break;
+			case TimerState.FINISHED:
+				setShortBreakTimer(shortBreakInitialTimer);
+				setShortBreakIsRunning(true);
+				setShortBreakState(TimerState.RUNNING);
+				break;
+		}
+	};
+
+	const manageLongBreakTimer = (state: TimerState) => {
+		switch (state) {
+			case TimerState.RUNNING:
+				setLongBreakIsRunning(false);
+				setLongBreakState(TimerState.PAUSED);
+				break;
+			case TimerState.PAUSED:
+				setLongBreakIsRunning(true);
+				setLongBreakState(TimerState.RUNNING);
+
+				setPomodoroIsRunning(false);
+				setPomodoroState(TimerState.PAUSED);
+				setShortBreakIsRunning(false);
+				setShortBreakState(TimerState.PAUSED);
+				break;
+			case TimerState.FINISHED:
+				setLongBreakTimer(longBreakInitialTimer);
+				setLongBreakIsRunning(true);
+				setLongBreakState(TimerState.RUNNING);
 				break;
 		}
 	};
@@ -76,77 +152,162 @@ export default function Home() {
 					<Image src="logo.svg" width={157} height={40} alt="pomodoro logo" />
 				</div>
 				<ol className="flex justify-center items-center p-3 bg-gunmetal rounded-full text-lightPeriwinkle font-bold text-[14px]">
-					<li className="bg-lightCoral px-6 py-4 rounded-full text-spaceCadet cursor-pointer">
+					<li
+						className={cn("px-6 py-4 rounded-full cursor-pointer", {
+							"bg-lightCoral text-spaceCadet": tab === 0,
+						})}
+						onClick={() => setTab(0)}
+					>
 						pomodoro
 					</li>
-					<li className="px-6 py-4 rounded-full cursor-pointer">short break</li>
-					<li className="px-6 py-4 rounded-full cursor-pointer">long break</li>
+					<li
+						className={cn("px-6 py-4 rounded-full cursor-pointer", {
+							"bg-lightCoral text-spaceCadet": tab === 1,
+						})}
+						onClick={() => setTab(1)}
+					>
+						short break
+					</li>
+					<li
+						className={cn("px-6 py-4 rounded-full cursor-pointer", {
+							"bg-lightCoral text-spaceCadet": tab === 2,
+						})}
+						onClick={() => setTab(2)}
+					>
+						long break
+					</li>
 				</ol>
 				<div
-					className="flex items-center justify-center w-full h-full cursor-pointer"
-					onClick={() => managePomodoroTimer(pomodoroState)}
+					className={cn({
+						hidden: tab !== 0,
+					})}
 				>
-					<div
-						className="flex items-center justify-center relative aspect-square w-[300px] md:w-[410px] rounded-full bg-gradient-to-tl from-[#2e325a] to-[#0e112a]"
-						style={{
-							boxShadow:
-								"-50px -50px 100px 0px #272C5A, 50px 50px 100px 0px #121530",
-						}}
-					>
-						<div className="aspect-square w-full bg-gunmetal rounded-full m-4 ">
-							<div className="absolute inset-0 flex items-center justify-center">
-								<svg
-									width={410}
-									height={410}
-									viewBox="0 0 410 410"
-									className={cn({
-										"opacity-0": !pomodoroIsVisible,
-										"opacity-100": pomodoroIsVisible,
-									})}
-								>
-									<circle
-										cx={410 / 2}
-										cy={410 / 2}
-										strokeWidth="10px"
-										r={radius}
-										className="cb stroke-gunmetal"
-									/>
-									<circle
-										cx={410 / 2}
-										cy={410 / 2}
-										strokeWidth="10px"
-										r={radius}
-										className="cp stroke-lightCoral"
-										style={{
-											strokeDasharray: dashArray,
-											strokeDashoffset: pomodoroDashOffset,
-										}}
-										transform={`rotate(-90 ${410 / 2} ${410 / 2})`}
-									/>
-								</svg>
-							</div>
-							<div
-								className={`${sans.className} flex flex-col h-full w-full items-center justify-center z-50 text-lightPeriwinkle`}
-							>
-								<p className="tracking-[-5px] text-[100px] font-bold">
-									{formatTime(pomodoroTimer)}
-								</p>
-								<p className="tracking-[15px] text-[16px] uppercase font-bold">
-									{pomodoroState}
-								</p>
-							</div>
-						</div>
-					</div>
+					<TimerTab
+						manageTimer={managePomodoroTimer}
+						formatTime={formatTime}
+						timer={pomodoroTimer}
+						timerState={pomodoroState}
+						isVisible={pomodoroIsVisible}
+						radius={radius}
+						dashArray={dashArray}
+						dashOffset={pomodoroDashOffset}
+					/>
 				</div>
 				<div
-					className="flex items-center justify-center cursor-pointer"
-					onClick={() => {
-						setPomodoroDuration(25);
-					}}
+					className={cn({
+						hidden: tab !== 1,
+					})}
 				>
+					<TimerTab
+						manageTimer={manageShortBreakTimer}
+						formatTime={formatTime}
+						timer={shortBreakTimer}
+						timerState={shortBreakState}
+						isVisible={shortBreakIsVisible}
+						radius={radius}
+						dashArray={dashArray}
+						dashOffset={shortBreakDashOffset}
+					/>
+				</div>
+				<div
+					className={cn({
+						hidden: tab !== 2,
+					})}
+				>
+					<TimerTab
+						manageTimer={manageLongBreakTimer}
+						formatTime={formatTime}
+						timer={longBreakTimer}
+						timerState={longBreakState}
+						isVisible={longBreakIsVisible}
+						radius={radius}
+						dashArray={dashArray}
+						dashOffset={longBreakDashOffset}
+					/>
+				</div>
+				<div className="flex items-center justify-center cursor-pointer">
 					<SettingsIcon />
 				</div>
 			</div>
 		</main>
 	);
 }
+
+const TimerTab = ({
+	manageTimer,
+	formatTime,
+	timer,
+	timerState,
+	isVisible,
+	radius,
+	dashArray,
+	dashOffset,
+}: {
+	manageTimer: (timerState: TimerState) => void;
+	formatTime: (time: number) => string;
+	timer: number;
+	timerState: TimerState;
+	isVisible: boolean;
+	radius: number;
+	dashArray: number;
+	dashOffset: number;
+}) => {
+	return (
+		<div
+			className="flex items-center justify-center w-full h-full cursor-pointer"
+			onClick={() => manageTimer(timerState)}
+		>
+			<div
+				className="flex items-center justify-center relative aspect-square w-[300px] md:w-[410px] rounded-full bg-gradient-to-tl from-[#2e325a] to-[#0e112a]"
+				style={{
+					boxShadow:
+						"-50px -50px 100px 0px #272C5A, 50px 50px 100px 0px #121530",
+				}}
+			>
+				<div className="aspect-square w-full bg-gunmetal rounded-full m-4 ">
+					<div className="absolute inset-0 flex items-center justify-center">
+						<svg
+							width={410}
+							height={410}
+							viewBox="0 0 410 410"
+							className={cn({
+								"opacity-0": !isVisible,
+								"opacity-100": isVisible,
+							})}
+						>
+							<circle
+								cx={410 / 2}
+								cy={410 / 2}
+								strokeWidth="10px"
+								r={radius}
+								className="cb stroke-gunmetal"
+							/>
+							<circle
+								cx={410 / 2}
+								cy={410 / 2}
+								strokeWidth="10px"
+								r={radius}
+								className="cp stroke-lightCoral"
+								style={{
+									strokeDasharray: dashArray,
+									strokeDashoffset: dashOffset,
+								}}
+								transform={`rotate(-90 ${410 / 2} ${410 / 2})`}
+							/>
+						</svg>
+					</div>
+					<div
+						className={`${sans.className} flex flex-col h-full w-full items-center justify-center z-50 text-lightPeriwinkle`}
+					>
+						<p className="tracking-[-5px] text-[100px] font-bold">
+							{formatTime(timer)}
+						</p>
+						<p className="tracking-[15px] text-[16px] uppercase font-bold">
+							{timerState}
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
