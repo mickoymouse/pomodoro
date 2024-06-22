d0f5dd52e7f84f2f2bdbf248a19adb6bcff4972b
mickoymouse <mckslvstr@gmail.com>
2024-06-22 13:27:46 +0800

    Done with initial markup.

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,16 @@
 import Image from "next/image";
 import { Kumbh_Sans } from "next/font/google";
 
+import SettingsIcon from "@/public/icon-settings.svg";
+
 const sans = Kumbh_Sans({ subsets: ["latin"] });
 export default function Home() {
 	const radius = 169.5;
 	const dashArray = radius * 2 * Math.PI;
-	const dashOffset = dashArray - (dashArray * 100) / 100;
+	const dashOffset = dashArray - (dashArray * 85) / 100;
 
 	return (
-		<main className="flex min-h-screen flex-col items-center justify-center">
+		<main className="flex min-h-screen flex-col items-center justify-center bg-spaceCadet">
 			<div className="container w-full flex flex-col gap-8">
 				<div className="w-full flex items-center justify-center">
 					<Image src="logo.svg" width={157} height={40} alt="pomodoro logo" />
@@ -19,39 +21,50 @@ export default function Home() {
 					<li>long break</li>
 				</ol>
 				<div className="flex items-center justify-center w-full h-full">
-					<div className="relative aspect-square w-[300px] md:w-[410px]">
-						<div className="absolute inset-0 flex items-center justify-center">
-							<svg width={410} height={410} viewBox="0 0 410 410">
-								<circle
-									cx={410 / 2}
-									cy={410 / 2}
-									strokeWidth="15px"
-									r={radius}
-									className="cb"
-								/>
-								<circle
-									cx={410 / 2}
-									cy={410 / 2}
-									strokeWidth="15px"
-									r={radius}
-									className="cp"
-									style={{
-										strokeDasharray: dashArray,
-										strokeDashoffset: dashOffset,
-									}}
-									transform={`rotate(-90 410/2 410/2)`}
-								/>
-							</svg>
-						</div>
-						<div
-							className={`${sans.className} flex flex-col h-full w-full items-center justify-center z-50`}
-						>
-							<p className="tracking-[-5px] text-[100px] font-bold">17:59</p>
-							<p className="tracking-[15px] text-[16px] uppercase font-bold">
-								pause
-							</p>
+					<div
+						className="flex items-center justify-center relative aspect-square w-[300px] md:w-[410px] rounded-full bg-gradient-to-tl from-[#2e325a] to-[#0e112a]"
+						style={{
+							boxShadow:
+								"-50px -50px 100px 0px #272C5A, 50px 50px 100px 0px #121530",
+						}}
+					>
+						<div className="aspect-square w-full bg-gunmetal rounded-full m-4">
+							<div className="absolute inset-0 flex items-center justify-center">
+								<svg width={410} height={410} viewBox="0 0 410 410">
+									<circle
+										cx={410 / 2}
+										cy={410 / 2}
+										strokeWidth="10px"
+										r={radius}
+										className="cb stroke-gunmetal"
+									/>
+									<circle
+										cx={410 / 2}
+										cy={410 / 2}
+										strokeWidth="10px"
+										r={radius}
+										className="cp stroke-lightCoral"
+										style={{
+											strokeDasharray: dashArray,
+											strokeDashoffset: dashOffset,
+										}}
+										transform={`rotate(-90 ${410 / 2} ${410 / 2})`}
+									/>
+								</svg>
+							</div>
+							<div
+								className={`${sans.className} flex flex-col h-full w-full items-center justify-center z-50 text-lightPeriwinkle`}
+							>
+								<p className="tracking-[-5px] text-[100px] font-bold">17:59</p>
+								<p className="tracking-[15px] text-[16px] uppercase font-bold">
+									pause
+								</p>
+							</div>
 						</div>
 					</div>
+				</div>
+				<div className="flex items-center justify-center">
+					<SettingsIcon />
 				</div>
 			</div>
 		</main>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,24 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		"./pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			colors: {
+				lightCoral: "#f87070",
+				electricBlue: "#70f3f8",
+				lavender: "#d881f8",
+				lightPeriwinkle: "#d7e0ff",
+				spaceCadet: "#1e213f",
+				lavenderMist: "#eff1fa",
+				gunmetal: "#161932",
+			},
+		},
+	},
+	plugins: [],
 };
 export default config;
